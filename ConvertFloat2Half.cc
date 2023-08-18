16ec886411ff2069675fefc895665bff443a63f1
Wang Bojun <105858416+wwbitejotunn@users.noreply.github.com>
2023-08-18 16:53:59 +0800

    Create ConvertFloat2Half.cc

---
+++ ConvertFloat2Half.cc
@@ -0,0 +1,9 @@
+uint as_uint(const float x) {
+    return *(uint*)&x;
+}
+uint16_t ConvertFloat2Half(const float x){
+    const uint b = as_uint(x)+0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
+    const uint e = (b&0x7F800000)>>23; // exponent
+    const uint m = b&0x007FFFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
+    return (b&0x80000000)>>16 | (e>112)*((((e-112)<<10)&0x7C00)|m>>13) | ((e<113)&(e>101))*((((0x007FF000+m)>>(125-e))+1)>>1) | (e>143)*0x7FFF; // sign : normalized : denormalized : saturate
+}
